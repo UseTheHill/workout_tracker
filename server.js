33dfb11e40d684bad8e659be6365d0c1577a012c
Kayte McDonough <kaytemcdonough@gmail.com>
2021-05-25 09:58:25 -0400

    started server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/tracker", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+  useUnifiedToplogy: true,
+});
