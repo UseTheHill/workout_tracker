9af1cf7b3df7d1a77c49e315c912c6221fefa404
Kayte McDonough <kaytemcdonough@gmail.com>
2021-05-25 10:27:48 -0400

    finished routes

--- routes/api.js
+++ routes/api.js
@@ -47,3 +47,17 @@ router.get("/api/workouts", (req, res) => {
       res.json(err);
     });
 });
+
+router.get("/exercise", function (req, res) {
+  res.sendFile(path.join(__dirname, "../public/exercise.html"));
+});
+
+router.get("/stats", function (req, res) {
+  res.sendFile(path.join(__dirname, "../public/stats.html"));
+});
+
+router.get("/exercise", function (req, res) {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+module.exports = router;
