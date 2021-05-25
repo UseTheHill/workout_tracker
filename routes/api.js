1099505eddec528c9e74ad277e36f7b13384313c
Kayte McDonough <kaytemcdonough@gmail.com>
2021-05-25 10:10:35 -0400

    added post router request

--- routes/api.js
+++ routes/api.js
@@ -2,3 +2,12 @@ const router = require("express").Router();
 const Workout = require("../models/workout.js");
 const path = require("path");
 
+router.post("/api/workouts", ({ body }, res) => {
+Workout.create({})
+.then((dbWorkout) => {
+    res.json(dbWorkout);
+})
+.catch(({ message }) => {
+    console.log(message);
+});
+});
