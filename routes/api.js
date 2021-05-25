8e938487d49b24db94745b115880c6f3f988afcf
Kayte McDonough <kaytemcdonough@gmail.com>
2021-05-25 10:13:52 -0400

    added put route

--- routes/api.js
+++ routes/api.js
@@ -3,11 +3,26 @@ const Workout = require("../models/workout.js");
 const path = require("path");
 
 router.post("/api/workouts", ({ body }, res) => {
-Workout.create({})
-.then((dbWorkout) => {
-    res.json(dbWorkout);
-})
-.catch(({ message }) => {
-    console.log(message);
+  Workout.create({})
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch(({ message }) => {
+      console.log(message);
+    });
 });
+
+router.put("/api/workouts/:id", ({ params, body }, res) => {
+  console.log("PARAMS", body, params);
+  Workout.findOneAndUpdate(
+    { __id: params.id },
+    { $push: { exercises: body } },
+    { new: true }
+  )
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
