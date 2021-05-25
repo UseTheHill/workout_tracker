c8ad1c9efea7b2b0ad48f3bd055fdf8e7db36e41
Kayte McDonough <kaytemcdonough@gmail.com>
2021-05-25 10:27:16 -0400

    added get rout for range and workouts

--- routes/api.js
+++ routes/api.js
@@ -26,3 +26,24 @@ router.put("/api/workouts/:id", ({ params, body }, res) => {
       res.json(err);
     });
 });
+
+router.get("/api/workouts/range", (req, res) => {
+  Workout.find({})
+    .limit(7)
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.get("/api/workouts", (req, res) => {
+  Workout.find({})
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
